4d9d60bb00905646d80e6bd527fc606ef71270a8
yuyanglin28 <annielin9908@gmail.com>
2019-09-16 10:09:02 +0800

    1 finish and 2 not

---
+++ lab2/ex1.c
@@ -0,0 +1,184 @@
+/*************************************
+ * Lab 2 Exercise 1
+ * Name:
+ * Student No:
+ * Lab Group:
+ *************************************
+ Warning: Make sure your code works on
+ lab machine (Linux on x86)
+ *************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>     //for fork(), wait()
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<sys/wait.h>
+
+void readFromFile (char who[50], int fd);
+void readOneFile (char who[50], int fd);
+void readFromBuffer(char who[50], FILE* fp);
+void readOneBuffer(char who[50], FILE* fp);
+
+int main(int argc, char **argv) {
+    char* fileName = NULL;
+    int nChild = 0;
+    int fd = 0;
+    char who[50];
+
+    if (argc < 3)
+    {
+        printf ("Usage: %s <filename> <number_of_processes>\n", argv[0]);
+        return 1;
+    }
+
+    fileName = argv[1];
+    nChild = atoi(argv[2]);
+
+    fd = open( fileName, O_RDONLY );
+
+    sprintf(who, "Parent [%d]", getpid());
+    //char doneMsg[128*nChild];
+    int ppid = getpid();
+    readOneFile(who, fd );
+
+    for (int i=0; i<nChild; i++){
+      if (fork() == 0){
+        sprintf(who, "Child %d[%d]", i+1, getpid());
+        break;
+      }
+    }
+    readFromFile(who, fd);
+
+    for (int i=0; i<nChild+1; i++){
+      if (getpid() != ppid){
+        char strBuf[128];
+        sprintf(strBuf, "Parent: %s done.\n", who );
+        exit(0);
+      }
+    }
+
+
+
+
+    for (int i=0; i<nChild; i++)
+      wait(NULL);
+
+    close (fd);
+//-----------------------------------------------------------------
+    FILE *fp;
+    fp = fopen(fileName, "r");
+    sprintf(who, "Parent [%d]", getpid());
+    readOneBuffer(who, fp );
+
+    for (int i=0; i<nChild; i++){
+      if (fork() == 0){
+        sprintf(who, "Child %d[%d]", i+1, getpid());
+        break;
+      }
+    }
+    readFromBuffer(who, fp);
+
+    for (int i=0; i<nChild+1; i++){
+      if (getpid() != ppid){
+        char strBuf[128];
+        sprintf(strBuf, "Parent: %s done.\n", who);
+        fwrite(&strBuf, sizeof(strBuf),1, stdout);
+        exit(0);
+      }
+    }
+
+    for (int i=0; i<nChild; i++)
+      wait(NULL);
+
+
+    printf("%s\n", "Parent: Exiting.");
+    fclose (fp);
+
+}
+
+//----------------------------------------------------------
+void readFromFile (char who[50], int fd)
+{
+	ssize_t readBytes = 1;
+	char charBuf = 0;
+  char strBuf[128];
+
+	while (readBytes > 0) {
+		usleep (1000);
+		charBuf = 0;
+		readBytes = read( fd, &charBuf, 1 );
+
+		if( readBytes != 1 ){
+			if( readBytes == 0 ){
+				return;
+			}
+		}
+
+    sprintf(strBuf, "%s: %c\n", who, charBuf);
+	  write (STDOUT_FILENO, strBuf, strlen(strBuf));
+
+	}
+
+}
+
+void readOneFile(char who[50], int fd){
+  ssize_t readBytes = 1;
+	char charBuf = 0;
+  char strBuf[128];
+
+		charBuf = 0;
+		readBytes = read( fd, &charBuf, 1 );
+		if( readBytes != 1 ){
+			if( readBytes == 0 ){
+				exit(0);
+			}
+		}
+    sprintf(strBuf, "%s: %c\n", who, charBuf);
+	  write (STDOUT_FILENO, strBuf, strlen(strBuf));
+
+}
+
+//--------------------------------------------------------------
+
+void readFromBuffer(char who[50], FILE* fp){
+  ssize_t readBytes = 1;
+	char charBuf = 0;
+  char strBuf[128];
+
+	while (readBytes > 0) {
+		//usleep (rand()%2000);
+    usleep(1000);
+		charBuf = 0;
+		readBytes = fread(&charBuf, 1 , 1, fp);
+
+		if( readBytes != 1 ){
+			if( readBytes == 0 ){
+				return;
+			}
+		}
+
+    sprintf(strBuf, "%s: %c\n", who, charBuf);
+	  fwrite(strBuf, strlen(strBuf),1, stdout);
+
+	}
+
+}
+
+void readOneBuffer(char who[50], FILE* fp){
+  ssize_t readBytes = 1;
+	char charBuf = 0;
+  char strBuf[128];
+
+		charBuf = 0;
+		readBytes = fread(&charBuf, 1 , 1, fp);
+		if( readBytes != 1 ){
+			if( readBytes == 0 ){
+				exit(0);
+			}
+		}
+    sprintf(strBuf, "%s: %c\n", who, charBuf);
+	  fwrite(strBuf, strlen(strBuf),1, stdout);
+}
---
+++ lab2/ex2.c
@@ -0,0 +1,120 @@
+/*************************************
+ * Lab 2 Exercise 2
+ * Name:
+ * Student No:
+ * Lab Group:
+ *************************************
+ Warning: Make sure your code works on
+ lab machine (Linux on x86)
+ *************************************/
+
+#include <stdio.h>
+#include <fcntl.h>      //For stat()
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>   //for waitpid()
+#include <unistd.h>     //for fork(), wait()
+#include <string.h>     //for string comparison etc
+#include <stdlib.h>     //for malloc()
+
+char** readTokens(int maxTokenNum, int maxTokenSize, int* readTokenNum, char* buffer);
+void freeTokenArray(char** strArr, int size);
+
+int main() {
+
+    int max_argument = 10;
+    int max_length = 19;
+    char commandLine[max_argument*(max_length+1)];
+
+  while (1){
+
+    printf("%s","GENIE > " );
+    scanf("%s", commandLine);
+    int num;
+    char ** commandFull = readTokens(max_argument+1, max_length, &num, commandLine);
+
+    char* command = commandFull[0];
+    char quit[] = "quit";
+
+    if (num==1 && strcmp(command, quit)==0){
+      printf("%s\n", "Goodbye!");
+      exit(0);
+    }else{
+
+      if (fork() == 0){
+        system(commandLine);
+        exit(0);
+      }else {
+        wait(NULL);
+      }
+
+    }
+  }
+
+
+}
+
+
+
+
+
+
+char** readTokens(int maxTokenNum, int maxTokenSize, int* readTokenNum, char* buffer)
+//Tokenize buffer
+//Assumptions:
+//  - the tokens are separated by " " and ended by newline
+//Return: Tokenized substrings as array of strings
+//        readTokenNum stores the total number of tokens
+//Note:
+//  - should use the freeTokenArray to free memory after use!
+{
+    char** tokenStrArr;
+    char* token;
+    int i;
+
+    //allocate token array, each element is a char*
+    tokenStrArr = (char**) malloc(sizeof(char*) * maxTokenNum);
+
+    //Nullify all entries
+    for (int i = 0; i < maxTokenNum; i++) {
+        tokenStrArr[i] = NULL;
+    }
+
+    token = strtok(buffer, " \n");
+
+    i = 0;
+    while (i < maxTokenNum && (token != NULL)) {
+         //Allocate space for token string
+        tokenStrArr[i] = (char*) malloc(sizeof(char*) * maxTokenSize);
+
+        //Ensure at most 19 + null characters are copied
+        strncpy(tokenStrArr[i], token, maxTokenSize - 1);
+
+        //Add NULL terminator in the worst case
+        tokenStrArr[i][maxTokenSize-1] = '\0';
+
+        i++;
+        token = strtok(NULL, " \n");
+    }
+
+    *readTokenNum = i;
+
+    return tokenStrArr;
+}
+
+void freeTokenArray(char** tokenStrArr, int size) {
+    int i = 0;
+
+    //Free every string stored in tokenStrArr
+    for (i = 0; i < size; i++) {
+        if (tokenStrArr[i] != NULL) {
+            free(tokenStrArr[i]);
+            tokenStrArr[i] = NULL;
+        }
+    }
+    //Free entire tokenStrArr
+    free(tokenStrArr);
+
+    //Note: Caller still need to set the strArr parameter to NULL
+    //      afterwards
+}
