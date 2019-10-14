288ae51c9cd9a381f02ac0563c52a6215e4d99f8
yuyanglin28 <annielin9908@gmail.com>
2019-10-14 20:47:31 +0800

    ex3

--- lab3/ex3/ex3.c
+++ lab3/ex3/ex3.c
@@ -15,21 +15,53 @@
 extern int num_of_cars;
 extern int num_of_segments;
 
+typedef struct
+{
+  sem_t state;
+} seg_state_stuct;
+
+sem_t car_limit;
+seg_state_stuct *seg_state;
+
 void initialise()
 {
+  sem_init(&car_limit, 0, num_of_segments-1);
+  seg_state = malloc (sizeof (seg_state_stuct) * num_of_segments);
+  ensure_successful_malloc(seg_state);
+  for (int i=0; i<num_of_segments; i++)
+    sem_init(&seg_state[i].state, 0, 1);
 }
 
 void cleanup()
 {
+  sem_destroy(&car_limit);
+  for (int i=0; i<num_of_segments; i++)
+    sem_destroy(&seg_state[i].state);
+  free(seg_state);
 }
 
 void* car(void* car)
 {
-    //This function modeles a thread
-    //A car:
-    //   -should call enter_roundabout (...)
-    //   -followed by some calls to move_to_next_segment (...)
-    //   -finally call exit_roundabout (...)
+    car_struct * one_car;
 
-    pthread_exit(NULL);
+    one_car = (car_struct*)car;
+
+    sem_wait(&car_limit);
+    sem_wait(&seg_state[one_car->entry_seg].state);
+    enter_roundabout(one_car);
+    
+    while(one_car->current_seg != one_car->exit_seg){
+
+      sem_wait(&seg_state[NEXT(one_car->current_seg, num_of_segments)].state);
+      move_to_next_segment(one_car);
+      sem_post(&seg_state[PREV(one_car->current_seg, num_of_segments)].state);
+
+    }
+    exit_roundabout(one_car);
+    sem_post(&seg_state[one_car->exit_seg].state);
+
+    sem_post(&car_limit);
+
+
+    pthread_exit(0);
 }
